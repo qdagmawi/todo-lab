e3faef071ab4d96f235b36aab567686975fcde29
Asrat <asratextras77@gmail.com>
2025-04-23 09:00:02 +0300

    Add cannot_request_one_self validation

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,6 +1,7 @@
 class FriendRequest < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
   belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
+  validate :cannot_request_one_self
 
   validates :status, presence: true
   enum :status, {
@@ -8,4 +9,8 @@ class FriendRequest < ApplicationRecord
     accepted: 1,
     rejected: 2
   }
+
+  def cannot_request_one_self
+    errors.add(:base, 'Cannot request oneself') if sender == receiver
+  end
 end
