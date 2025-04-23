20651c63710f8eed42dc29a4b61cbad849d39ba6
Asrat <asratextras77@gmail.com>
2025-04-23 07:04:02 +0300

    Add model User

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :password_digest, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+
+end
